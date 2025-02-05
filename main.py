b001bb26221f04aef8f267449eb084937851ef0f
ゆゆゆ <yuyuyu1226.m.1226@gmail.com>
2025-02-06 07:17:49 +0900

    Update main.py

--- main.py
+++ main.py
@@ -549,6 +549,15 @@ def toggleVideoCheck():
     invidious_api.check_video = not invidious_api.check_video
     return f'{not invidious_api.check_video} to {invidious_api.check_video}'
 
+@app.get("/verify", response_class=HTMLResponse)
+def get_form(seed=""):
+    return requests.get(fr"{url}verify?seed={urllib.parse.quote(seed)}").text
+
+@app.post("/submit", response_class=HTMLResponse)
+def submit(h_captcha_response: str = Form(alias="h-captcha-response"), seed: str = Form(...)):
+    return requests.post(fr"{url}submit",data={"h-captcha-response": h_captcha_response, "seed": seed}).text
+
+
 
 @app.exception_handler(500)
 def error500(request: Request, __):
